72da878483909783b09bf9aeddb3c36728197621
Guido <ppapaguido@gmail.com>
2021-11-04 15:15:41 +0100

    fine js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let nome = prompt('Inserisci il tuo nome');
+console.log(nome);
+
+let cognome = prompt('Inserisci il tuo cognomenome');
+console.log(cognome);
+
+let colore = prompt ('Inserisci il tuo colore preferito');
+console.log(colore);
+
+
+let pw = nome + cognome + colore + '21';
+
+console.log(pw);
+
+document.getElementById('ur-pw').innerHTML = pw;
