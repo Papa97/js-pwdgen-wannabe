b52a62e7f3bf2468b8b9fd10c06df7ecd1ecba37
Guido <ppapaguido@gmail.com>
2021-11-04 15:35:22 +0100

    aggiunta di un minimo di css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let nome = prompt('Inserisci il tuo nome');
 console.log(nome);
 
-let cognome = prompt('Inserisci il tuo cognomenome');
+let cognome = prompt('Inserisci il tuo cognome');
 console.log(cognome);
 
 let colore = prompt ('Inserisci il tuo colore preferito');
